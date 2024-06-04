558fef7abb0336887eda881ef6501f327833266b
yinqiao <1737666820@qq.com>
2024-06-04 22:43:39 +0800

    feat: 数组新虚拟dom diff 方案一实现完成（一共三种方案）

--- packages/react-reconciler/src/ReactChildFiber.js
+++ packages/react-reconciler/src/ReactChildFiber.js
@@ -110,10 +110,80 @@ function createChildReconciler(shouldTrackSideEffects) {
     return null
   }
 
-  function reconcileChildrenArray(returnFiber, currentFirstFiber, newChildren) {
+  /**
+   * 更新元素节点
+   * @param {Object} returnFiber 父级fiber节点
+   * @param {Object} current 当前fiber节点
+   * @param {Object} element 新的React元素
+   * @return {Object} 返回一个更新后的fiber节点
+   */
+  function updateElement(returnFiber, current, element) {
+    const elementType = element.type
+    if (current !== null) {
+      if (current.type === elementType) {
+        const existing = useFiber(current, element.props)
+        existing.return = returnFiber
+        return existing
+      }
+    }
+    const created = createFiberFromElement(element)
+    created.return = returnFiber
+    return created
+  }
+
+  /**
+   * 更新一个slot，如果新的子节点和旧的fiber节点匹配，则返回更新后的fiber节点，否则返回null
+   * @param {Object} returnFiber 父级fiber节点
+   * @param {Object|null} oldFiber 旧的fiber节点
+   * @param {any} newChild 新的子节点
+   * @return {Object|null} 返回一个更新后的fiber节点，如果新的子节点和旧的fiber节点不匹配，则返回null
+   */
+  function updateSlot(returnFiber, oldFiber, newChild) {
+    const key = oldFiber !== null ? oldFiber.key : null
+    if (newChild !== null && typeof newChild === 'object') {
+      switch (newChild.$$typeof) {
+        case REACT_ELEMENT_TYPE:
+          if (newChild.key === key) {
+            return updateElement(returnFiber, oldFiber, newChild)
+          }
+        default:
+          return null
+      }
+    }
+
+    return null
+  }
+
+  function reconcileChildrenArray(returnFiber, currentFirstChild, newChildren) {
     let resultingFirstChild = null
     let previousNewFiber = null
     let newIdx = 0
+    let oldFiber = currentFirstChild
+    let nextOldFiber = null
+
+    // 数组新虚拟DOM 第一套方案
+    for (; oldFiber !== null && newIdx < newChildren.length; newIdx++) {
+      nextOldFiber = oldFiber.sibling
+      // 这里生成的新fiber可以是复用原来的老fiber，也可能是新创建的
+      const newFiber = updateSlot(returnFiber, oldFiber, newChildren[newIdx])
+      if (newFiber === null) break
+      if (shouldTrackSideEffects) {
+        //  复用才有 alternate，没有复用就是null（因为新创建的fiber没有 alternate 值）
+        if (oldFiber && newFiber.alternate === null) {
+          deleteChild(returnFiber, oldFiber)
+        }
+      }
+      // lastPlacedIndex = placeChild(newFiber, lastPlacedIndex, newIdx)
+      if (previousNewFiber === null) {
+        resultingFirstChild = newFiber
+      } else {
+        previousNewFiber.sibling = newFiber
+      }
+      previousNewFiber = newFiber
+      oldFiber = nextOldFiber
+    }
+
+    // 数组新虚拟DOM 第二套方案
     for (; newIdx < newChildren.length; newIdx++) {
       const newFiber = createChild(returnFiber, newChildren[newIdx])
       if (newFiber === null) continue
@@ -126,6 +196,8 @@ function createChildReconciler(shouldTrackSideEffects) {
       }
       previousNewFiber = newFiber
     }
+
+    // 数组新虚拟DOM 第三套方案
     return resultingFirstChild
   }
 
@@ -137,6 +209,7 @@ function createChildReconciler(shouldTrackSideEffects) {
   }
 
   function reconcileChildFibers(returnFiber, currentFirstFiber, newChild) {
+    // 单节点 dom diff
     if (typeof newChild === 'object' && newChild !== null) {
       switch (newChild.$$typeof) {
         case REACT_ELEMENT_TYPE:
@@ -147,9 +220,12 @@ function createChildReconciler(shouldTrackSideEffects) {
           break
       }
     }
+
+    // 多节点 dom diff
     if (isArray(newChild)) {
       return reconcileChildrenArray(returnFiber, currentFirstFiber, newChild)
     }
+
     return null
   }
 
